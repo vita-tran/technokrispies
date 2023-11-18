21195063a17f792f35c27dc30593462da85f5be8
Rio Casanova <riocasanova@gmail.com>
2023-11-18 16:56:04 -0700

    update

---
+++ re-med-ner/components/NavBar.js
@@ -0,0 +1,3 @@
+export default function NavBar() {
+  <p>This is string</p>;
+}
--- re-med-ner/pages/backend/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Some PHP logic to process data
-$response = ['message' => 'Hello from PHP backend!'];
-
-// Sending data back to React in JSON format
-header('Content-Type: application/json');
-echo json_encode($response);
-?>
