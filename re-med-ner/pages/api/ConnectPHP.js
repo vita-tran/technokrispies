7289b89c2d01d67578aed6d5efa6f8e6130134dc
Rio Casanova <riocasanova@gmail.com>
2023-11-19 01:19:08 -0700

    changes to connection configuration

--- re-med-ner/pages/api/ConnectPHP.js
+++ re-med-ner/pages/api/ConnectPHP.js
@@ -2,23 +2,30 @@
 import React, { useState, useEffect } from "react";
 
 function ConnectPHP() {
-  const [message, setMessage] = useState("");
+  const [customer, setCustomer] = useState();
 
   useEffect(() => {
     // Fetch data from PHP backend when the component mounts
-    fetch(
-      "http://localhost/projects/technokrispies/re-med-ner/backend/connection.php"
-    )
+    fetch("http://rcasanova2.dmitstudent.ca/dmit2025/connection.php")
       .then((response) => response.json())
       .then((data) => {
-        setMessage(data.message);
+        setCustomer(data);
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
     <div className="">
-      <h1 className="">{message}</h1>
+      {customer ? (
+        <div>
+          <h1 className="">{customer.first_name}</h1>
+          <p>ID: {customer.customer_id}</p>
+          <p>Name: {customer.customer_name}</p>
+          <p>Email: {customer.email}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
