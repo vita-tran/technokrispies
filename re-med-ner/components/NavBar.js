b23c2d23c46e5183d9a4cacb9bf6ec49ca5c2370
Vita <vitatran1999@gmail.com>
2023-11-18 17:57:08 -0700

    Update NavBar.js

--- re-med-ner/components/NavBar.js
+++ re-med-ner/components/NavBar.js
@@ -6,39 +6,21 @@ import Toolbar from '@mui/material/Toolbar';
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from '@mui/material';
 
-// export default function NavBar(){
-//     return <AppBar position="static">
-//         <Toolbar>
-//             <IconButton
-//                 size="large"
-//                 edge="start"
-//                 color='inherit'
-//                 aria-label="menu"
-//                 sx={{ mr:2 }}
-//             >
-//                 <MenuIcon />
-//             </IconButton>
-            
-//         </Toolbar>
-//     </AppBar>
-// }
-
-
-// testing
-export default function NavBar(props) {
+export default function NavBar(){
     return <AppBar position="static">
-      <Toolbar>
-        <Link href="/">
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Re-Med-Ner
-          </Typography>
-        </Link>
-        <Link href="/about">
-          <Typography variant="h6" component="div" >
-            Our Goal
-          </Typography>
-        </Link>
-      </Toolbar>
+        <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color='inherit'
+                aria-label="menu"
+                sx={{ mr:2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            
+        </Toolbar>
     </AppBar>
-  }
+}
+
   
