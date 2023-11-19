d6e028f74d80f5ac6c0bc4435f5aee88133c0ac0
Vita <vitatran1999@gmail.com>
2023-11-18 21:24:25 -0700

    Updating form

--- re-med-ner/pages/index.js
+++ re-med-ner/pages/index.js
@@ -1,12 +1,17 @@
 import Head from "next/head";
-// import Image from "next/image";
+
 
 // ============================================
 // Importing MUI components below
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { useState, useEffect } from "react";
+
+
+import FormControl from '@mui/material/FormControl';
+
 // ============================================
 
 import NavBar from "../components/NavBar";
@@ -28,6 +33,50 @@ export default function Home() {
       {/* Here is where I am going to put the component for PHP */}
       <ConnectPHP />
       <NavBar />
+
+      <Container sx={{paddingTop:2}} component="main" maxWidth="lg">
+        <Typography variant="h4">
+          User Medical Information Form
+        </Typography>
+
+        <Box
+          component="form"
+          sx={{ width: 1 }}>
+            
+            <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="First Name"
+              sx={{ width: '40%', m: '2rem' }}
+
+            />
+
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="Last Name"
+              sx={{ width: '40%', m: '2rem' }}
+            />
+
+            {/*  PHONE NUMBER OR EMAIL HERE  */}
+
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="Last Name"
+            />
+
+
+
+
+            </div>
+        </Box>
+
+      </Container>
     </div>
   );
 }
