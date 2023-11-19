626ac85ce4144615c173fcce6f9ed15d7f5e64a5
Vita <vitatran1999@gmail.com>
2023-11-18 17:12:16 -0700

    Created NavBar.js in components folder

---
+++ re-med-ner/components/NavBar.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function NavBar(){
+    <p>Testing purposes</p>
+}
